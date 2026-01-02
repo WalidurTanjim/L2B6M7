98b3cbdd5bee992e9eb7c961d84d76691bfe8d9d
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-02 22:18:11 +0600

    Student class extends Person

--- dist/inheritance.js
+++ dist/inheritance.js
@@ -1,7 +1,41 @@
 "use strict";
+// // Parent class
+// class Parent {
+//     name: string;
+//     age: number;
+//     address: string;
 Object.defineProperty(exports, "__esModule", { value: true });
-// Parent class
-class Parent {
+//     constructor(name: string, age: number, address: string){
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     };
+//     getStudy(value: number) {
+//         console.log(`${this.name} study ${value} hours`);
+//     }
+// }
+// // Student class
+// class Student extends Parent {}
+// // Teacher class
+// class Teacher extends Parent {
+//     designation: string;
+//     constructor(name: string, age: number, address: string, designation: string){
+//         super(name, age, address); // parent class er constructor k call dey. that means 'name, age, address' k parent constructor k diye dey
+//         this.designation = designation;
+//     }
+//     takeClass(value: number) {
+//         console.log(`${this.name} take class ${value} hours`)
+//     }
+// }
+// const student1 = new Student("Walidur Tanjim", 25, "Mirpur, Dhaka");
+// console.log(student1);
+// student1.getStudy(5);
+// const teacher1 = new Teacher("Walidur Rahman", 25, "Mirpur, Dhaka", "Senior Teacher");
+// console.log(teacher1);
+// teacher1.takeClass(3);
+// =============================================================
+// Person class
+class Person {
     name;
     age;
     address;
@@ -12,26 +46,13 @@ class Parent {
     }
     ;
     getStudy(value) {
-        console.log(`${this.name} study ${value} hours`);
+        console.log(`${this.name} study ${value} hours.`);
     }
 }
 // Student class
-class Student extends Parent {
+class Student extends Person {
 }
-// Teacher class
-class Teacher extends Parent {
-    designation;
-    constructor(name, age, address, designation) {
-        super(name, age, address); // parent class er constructor k call dey. that means 'name, age, address' k parent constructor k diye dey
-        this.designation = designation;
-    }
-    takeClass(value) {
-        console.log(`${this.name} take class ${value} hours`);
-    }
-}
+;
 const student1 = new Student("Walidur Tanjim", 25, "Mirpur, Dhaka");
 console.log(student1);
-student1.getStudy(5);
-const teacher1 = new Teacher("Walidur Rahman", 25, "Mirpur, Dhaka", "Senior Teacher");
-console.log(teacher1);
-teacher1.takeClass(3);
+student1.getStudy(8);
