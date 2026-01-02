07369fdbf75a1a1f351aee33ad23dbc9e2265aba
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-03 02:19:26 +0600

    Protected access modifiers added

---
+++ dist/access_modifiers.js
@@ -0,0 +1,34 @@
+"use strict";
+// Access modifiers
+Object.defineProperty(exports, "__esModule", { value: true });
+class BankAccount {
+    accountNumber;
+    accountHolder;
+    balance;
+    accountType;
+    // constructor
+    constructor(accountNumber, accountHolder, balance, accountType) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+    ;
+    // methods
+    updateBalance(value) {
+        this.balance = this.balance + value;
+        console.log(`Total balance is: ${this.balance}`);
+    }
+}
+;
+// StudentBankAccount class
+class StudentBankAccount extends BankAccount {
+    test() {
+        console.log(`Student account balance is: ${this.balance}`);
+    }
+}
+// instance
+const walidurTanjim = new BankAccount(123, 'Walidur Tanjim', 100000, "Savings");
+walidurTanjim.updateBalance(5500);
+const studentAcount = new StudentBankAccount(123, "Student Walid", 50000, "Savings");
+studentAcount.test();
